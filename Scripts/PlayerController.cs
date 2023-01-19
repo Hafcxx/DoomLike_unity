f156b4b3919772be381e8b525bea9e247dcdd820
Claudio Aniñir <claudio.aninir01@gmail.com>
2023-01-18 23:02:39 -0300

    player movement basics

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     private Vector2 moveInput;
     private Vector2 mouseInput;
+    public Camera viewCam;
 
     public float mouseSensitivity = 1f; 
     void Start()
@@ -19,8 +20,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //PLAYER MOVEMENT
         moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        theRB.velocity = moveInput * moveSpeed; 
+        Vector3 moveHorizontal = transform.up * moveInput.y;
+
+        Vector3 moveVertical = transform.right * moveInput.x;
+
+        theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+
+        
+
+        //MOUSE
+        mouseInput = new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            transform.rotation.eulerAngles.y,
+            transform.rotation.eulerAngles.z - mouseInput.x);
+
+        viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+
+        //shooting
+
+        if (Input.GetMouseButton(0))
+        {   
+            //Estamos enviando un rayo, despues consultamos si este golpeó
+            Ray ray = viewCam.ViewportPointToRay(new Vector3(.5f, .5f, .5f));
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Im HITTING" + hit.transform.name);
+            }else
+            {
+                Debug.Log("Not hitting");
+            }
+        }
     }
 }
